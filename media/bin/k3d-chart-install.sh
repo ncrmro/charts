d22430adc1a5c51fe18f9bf802a0768f939f355a
Nicholas Romero <ncrmro@gmail.com>
2023-10-29 11:39:31 -0500

    feat(media/jellyfin): test helm chart

--- media/bin/k3d-chart-install.sh
+++ media/bin/k3d-chart-install.sh
@@ -1,5 +1,6 @@
 #helm --kube-context k3d-ncrmro-charts --namespace media uninstall media
-helm --kube-context k3d-ncrmro-charts --namespace media upgrade --install --create-namespace media .. \
+helm --kube-context k3d-ncrmro-charts --namespace media upgrade --install --create-namespace --wait --debug media .. \
 --set global.persistence.media.storageClass=local-path \
 --set jellyfin.persistence.data.storageClass=local-path \
 --set jellyfin.persistence.temp.storageClass=local-path
+helm --kube-context k3d-ncrmro-charts --namespace media test media
