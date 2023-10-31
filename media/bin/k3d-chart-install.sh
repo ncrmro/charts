afb82d758e22e20dad032646f725bcab397417de
Nicholas Romero <ncrmro@gmail.com>
2023-10-30 19:32:06 -0500

    feat(media/jellyfin): use built user abc with UID 911 from existing linuxer server image and better align pvc claims

--- media/bin/k3d-chart-install.sh
+++ media/bin/k3d-chart-install.sh
@@ -2,5 +2,6 @@
 helm --kube-context k3d-ncrmro-charts --namespace media upgrade --install --create-namespace --wait --debug media .. \
 --set global.persistence.media.storageClass=local-path \
 --set jellyfin.persistence.data.storageClass=local-path \
---set jellyfin.persistence.temp.storageClass=local-path
+--set jellyfin.persistence.temp.storageClass=local-path \
+--set jellyfin.ingress.enabled=true
 helm --kube-context k3d-ncrmro-charts --namespace media test media
